9afa55be8c7eccd9dc810ed4a28bbdb3214eb683
Alona Ihnatchenko <olena1366@gmail.com>
2022-11-29 00:06:39 +0200

    update 01 color switcher js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+const btnStart = document.querySelector('.data-start');
+const btnStop = document.querySelector('.data-stop');
+
+let timerId = null;
+
+btnStart.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+});
+
+btnStop.addEventListener('click', () => {
+    clearInterval(timerId);
+    btnStop.disabled = true;
+    btnStart.disabled = false;
+});
